f54633f9e50f319a8c8ff2da3149029c00d0822c
Mathias <email@example.com>
2024-08-12 12:37:51 +0000

    save

--- examples/csv_read_6_1.rs
+++ examples/csv_read_6_1.rs
@@ -7,27 +7,29 @@
 // use csv::Reader;
 use serde::Deserialize;
 
-
 // https://github.com/BurntSushi/rust-csv/issues/125
-
 
 // Date,Open,High,Low,Close,Volume
 
 #[derive(Debug, Deserialize)]
 struct Record {
-    #[allow(non_snake_case)]
     #[serde(rename = "Date")]
     date: String,
-    #[allow(non_snake_case)]
-    Open: f32,
-    #[allow(non_snake_case)]
-    High: f32,
-    #[allow(non_snake_case)]
-    Low: f32,
-    #[allow(non_snake_case)]
-    Close: f32,
-    #[allow(non_snake_case)]
-    Volume: f32,
+
+    #[serde(rename = "Open")]
+    open: f32,
+
+    #[serde(rename = "High")]
+    high: f32,
+
+    #[serde(rename = "Low")]
+    low: f32,
+
+    #[serde(rename = "Close")]
+    close: f32,
+
+    #[serde(rename = "Volume")]
+    volume: f32,
 }
 
 fn main() {
@@ -37,19 +39,13 @@ fn main() {
     //     let record = result.unwrap();
     //     println!("{:?}", record);
 
-
     // https://stackoverflow.com/questions/75287355/reading-csv-file-when-header-is-present-with-lower-or-upper-case
 
     for record in reader.deserialize() {
         let record: Record = record.unwrap();
         println!(
             " {:?}, {:?}, {:?}, {:?},{:?}, {:?}",
-            record.date,
-            record.open,
-            record.High,
-            record.Low,
-            record.Close,
-            record.Volume,
+            record.date, record.open, record.high, record.low, record.close, record.volume,
         );
     }
 }
