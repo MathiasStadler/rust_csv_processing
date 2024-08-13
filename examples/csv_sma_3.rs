1970e813abb06a12c7d5f5004d912ba2d8f7aa87
Mathias <email@example.com>
2024-08-13 07:39:05 +0000

    remove unnessery code

--- examples/csv_sma_3.rs
+++ examples/csv_sma_3.rs
@@ -11,50 +11,39 @@ use ta::indicators::SimpleMovingAverage as Sma;
 use ta::DataItem;
 use ta::Next;
 
-use serde::Deserialize;
+// use serde::Deserialize;
 
 // https://github.com/BurntSushi/rust-csv/issues/125
 
 // Date,Open,High,Low,Close,Volume
 
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[serde(rename = "Date")]
-    date: String,
+// #[derive(Debug, Deserialize)]
+// struct Record {
+//     #[serde(rename = "Date")]
+//     date: String,
 
-    #[serde(rename = "Open")]
-    open: f32,
+//     #[serde(rename = "Open")]
+//     open: f32,
 
-    #[serde(rename = "High")]
-    high: f32,
+//     #[serde(rename = "High")]
+//     high: f32,
 
-    #[serde(rename = "Low")]
-    low: f32,
+//     #[serde(rename = "Low")]
+//     low: f32,
 
-    #[serde(rename = "Close")]
-    close: f32,
+//     #[serde(rename = "Close")]
+//     close: f32,
 
-    #[serde(rename = "Volume")]
-    volume: f32,
-}
+//     #[serde(rename = "Volume")]
+//     volume: f32,
+// }
 
 fn main() {
-    let mut sma = Sma::new(7).unwrap();
+    let mut sma_7 = Sma::new(7).unwrap();
     let mut reader = csv::Reader::from_path("data/trex_us_d.csv").unwrap();
     // FROM HERE - https://docs.rs/csv/latest/csv/struct.Writer.html
-    let mut wtr = Writer::from_path("output_2.csv").unwrap();
-
-    // add sma
-
-    // for record in reader.deserialize() {
-
-    //     let record: Record = record.unwrap();
-
-    //     println!(
-    //         " {:?}, {:?}, {:?}, {:?},{:?}, {:?}",
-    //         record.date, record.open, record.high, record.low, record.close, record.volume,
-    //     );
-    // }
+    let mut wtr = Writer::from_path("output_sma_7.csv").unwrap();
+   
 
     wtr.write_record(&["Date", "Open", "High", "Low", "Close", "Volume", "SMA(7)"])
         .unwrap();
@@ -71,11 +60,13 @@ fn main() {
             .build()
             .unwrap();
 
-        let sma_val = sma.next(&dt);
-        // println!("{}: {} = {:2.2}", date, sma, sma_val);
+        let sma_7_val = sma_7.next(&dt);
+        // println!("{}: {} = {:2.2}", date, sma_7, sma_7_val
+        
         println!(
             " {:?}, {:?}, {:?}, {:?},{:?}, {:?}, {:2.2}",
-            date, open, high, low, close, volume, sma_val
+            date, open, high, low, close, volume, sma_7_val
+
         );
         wtr.write_record(&[
             date,
@@ -84,15 +75,13 @@ fn main() {
             low.to_string(),
             close.to_string(),
             volume.to_string(),
-            sma_val.to_string(),
+            sma_7_val
+            .to_string(),
         ])
         .unwrap();
-        // wtr.write_record(
-        //     " {:?}, {:?}, {:?}, {:?},{:?}, {:?}, {:2.2}",
-        //     date, open, high, low, close, volume, sma_val
-        // );
-        // println!("{}: {} = {:2.2}", date, sma, sma_val);
+        
+        
     }
 }
 
-// cargo run --example csv_sma_2
+// cargo run --example csv_sma_3
